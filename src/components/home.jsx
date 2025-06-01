c239cdd8f1dae14415c0ab70f9fe97b2c390f482
GresterMarak <grestarr497@gmail.com>
2025-06-01 15:23:32 +0530

    Add initial files

---
+++ src/components/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '/src/App.css'
+
+const Home = () => {
+    return(
+        <>
+        <div className='body-container'>
+            <div className='header'>
+                <h1>Jarvis</h1>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Home;
